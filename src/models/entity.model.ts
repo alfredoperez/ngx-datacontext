ee35783e1b89ce28d44829374528db2bc220f3de
Alfredo Perez <alfredo.perez.q@gmail.com>
2017-11-01 08:42:51 -0500

    fix(entity): Fix lint problems

--- src/models/entity.model.ts
+++ src/models/entity.model.ts
@@ -1,7 +1,6 @@
 import * as _ from 'lodash';
 
 export class BaseEntity {
-
   public entityName: string;
   public key: string;
 
@@ -9,50 +8,54 @@ export class BaseEntity {
     this.entityName = (this as any).$$name;
   }
 
-  public deserialize(input: Object): BaseEntity {
-    for ( const property in input ) {
-      if ( !input.hasOwnProperty(property) ) {
+  public deserialize(input: any): BaseEntity {
+    for (const property in input) {
+      if (!input.hasOwnProperty(property)) {
         continue;
       }
 
-      if ( typeof this[property] === 'object' && input[property] !== undefined ) {
-        const result = this.deserializeEntity(this[property], input[property]);
-        if ( Array.isArray(result) ) {
-          this[property] = result.splice(0);
-        }        else {
-          this[property] = Object.assign({}, result);
+      if (
+        typeof (this as any)[property] === 'object' &&
+        input[property] !== undefined
+      ) {
+        const result = this.deserializeEntity(
+          (this as any)[property],
+          input[property]
+        );
+        if (Array.isArray(result)) {
+          (this as any)[property] = result.splice(0);
+        } else {
+          (this as any)[property] = Object.assign({}, result);
         }
-
-      } else if ( this.isDate(input[property]) ) {
-        this[property] = new Date(input[property]);
+      } else if (this.isDate(input[property])) {
+        (this as any)[property] = new Date(input[property]);
       } else {
-        this[property] = input[property];
+        (this as any)[property] = input[property];
       }
-
     }
 
     return this;
   }
 
   private deserializeEntity(entity: any, data: any) {
-    if ( entity === undefined || data === undefined ) {
+    if (entity === undefined || data === undefined) {
       return undefined;
     }
 
     let result;
-    if ( Array.isArray(data) ) {
+    if (Array.isArray(data)) {
       const collection = [];
-      for ( const d in data ) {
-        if ( d !== undefined ) {
+      for (const d in data) {
+        if (d !== undefined) {
           const item = typeof data === 'object' ? data[d] : data;
           const deserialize = entity.deserialize(item);
           collection.push(deserialize);
         }
       }
       result = collection;
-    } else if ( !_.isEmpty(entity) && entity.deserialize !== undefined ) {
+    } else if (!_.isEmpty(entity) && entity.deserialize !== undefined) {
       result = entity.deserialize(data);
-    } else if ( !_.isEmpty(entity) ) {
+    } else if (!_.isEmpty(entity)) {
       result = data;
     }
 
@@ -60,13 +63,14 @@ export class BaseEntity {
   }
 
   private isDate(field: any) {
-    return typeof field === 'string'
-      && field.indexOf('Z') !== -1
-      && !isNaN(Date.parse(field));
+    return (
+      typeof field === 'string' &&
+      field.indexOf('Z') !== -1 &&
+      !isNaN(Date.parse(field))
+    );
   }
 
   isNew(): boolean {
     return this.key === undefined;
   }
 }
-
