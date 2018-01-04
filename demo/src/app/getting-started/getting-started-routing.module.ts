15f2ae01727b77b55d105b4edcad83d312a5a9d0
Alfredo Perez <alfredo.perez.q@gmail.com>
2018-01-04 06:24:21 -0600

    feat(demo): fix lint errors

--- demo/src/app/getting-started/getting-started-routing.module.ts
+++ demo/src/app/getting-started/getting-started-routing.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 import { GettingStartedComponent } from './getting-started.component';
 
 @NgModule({
-    imports: [RouterModule.forChild([
-      { path: '', component: GettingStartedComponent }
-    ])],
-    exports: [RouterModule]
-  })
-export class GettingStartedRoutingModule {}
+  imports: [RouterModule.forChild([
+    { path: '', component: GettingStartedComponent }
+  ])],
+  exports: [RouterModule]
+})
+export class GettingStartedRoutingModule { }
--- demo/src/app/home/home.component.ts
+++ demo/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Title }     from '@angular/platform-browser';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +8,7 @@ import { Title }     from '@angular/platform-browser';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private titleService:Title) { }
+  constructor(private titleService: Title) { }
 
   ngOnInit() {
     this.titleService.setTitle('Home | ngx-datacontext');
